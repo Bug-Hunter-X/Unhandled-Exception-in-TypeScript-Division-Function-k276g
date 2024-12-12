9d4350231c845da9e6b4c4c1b4fbe6901da3d392
BugHunter-X <askzenai@gmail.com>
2024-12-12 07:34:06 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,23 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+console.log(add(5,3));
+console.log(subtract(10, 5));
+console.log(multiply(4, 6));
+console.log(divide(10, 0));
